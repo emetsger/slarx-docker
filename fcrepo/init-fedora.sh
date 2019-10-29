21d56a11f4e4bec61e0ddc72907372d12668f02a
Elliot Metsger <emetsger@jhu.edu>
2019-10-28 21:29:46 -0400

    Max tries

--- fcrepo/init-fedora.sh
+++ fcrepo/init-fedora.sh
@@ -19,7 +19,7 @@ then
   apk -q --no-progress add curl
   java -jar start.jar -Djetty.http.port=8080 2>/dev/null 1>/dev/null &
 
-  fedora_up
+  fedora_up 10
 
   if [ $? -ne 0 ] ;
   then
--- fcrepo/run_tests.sh
+++ fcrepo/run_tests.sh
@@ -8,7 +8,7 @@ source /fedora-env.sh
 apk -q --no-progress add curl
 /init-fedora.sh 2>/run_tests.err 1>run_tests.out &
 
-fedora_up
+fedora_up 20
 
 if [ $? -ne 0 ] ;
 then
