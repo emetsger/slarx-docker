243a49fc6ecfced8902644df60fce9650037864c
Elliot Metsger <emetsger@jhu.edu>
2019-10-29 00:08:20 -0400

    boo

--- fcrepo/run_tests.sh
+++ fcrepo/run_tests.sh
@@ -6,14 +6,14 @@ echo "Running tests..."
 
 source /fedora-env.sh
 apk -q --no-progress add curl
-/init-fedora.sh 2>/run_tests.err 1>run_tests.out &
+/init-fedora.sh 2>/tmp/run_tests.err 1>/tmp/run_tests.out &
 
 fedora_up 30
 
 echo "** run_tests.err **"
-cat /run_tests.err
+cat /tmp/run_tests.err
 echo "** run_tests.out **"
-cat /run_tests.out
+cat /tmp/run_tests.out
 
 if [ $? -ne 0 ] ;
 then
